9798b74e389b005ab1fdae92beb474480635e771
Ahmet Sezgin Duran <marjinalist1@gmail.com>
2013-09-28 12:44:13 +0300

    #1 Format JSON data as asked

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -6,11 +6,13 @@ class Api::V1::SessionsController < ApplicationController
     email = params[:email]
     password = params[:password]
 
+    # Checking whether format is json or not
     if request.format != :json
       render status: 406, json: { message: 'The request must be json'}
       return
     end
 
+    # Checking for empty email or password
     if email.nil? or password.nil?
       render status: 400, json: { message: 'The request must contain the user email and password' }
       return
@@ -18,6 +20,7 @@ class Api::V1::SessionsController < ApplicationController
 
     @user = User.find_by_email(email.downcase)
 
+    # Checking whether user exists
     if @user.nil?
       render status: 401, json: { message: 'Invalid email or password' }
       return
@@ -25,10 +28,14 @@ class Api::V1::SessionsController < ApplicationController
 
     @user.ensure_authentication_token!
 
+    # Finally, checking for password
     if not @user.valid_password?(password)
       render status: 401, json: { message: 'Invalid email or password.'}
     else
-      render status: 200, json: { token: @user.authentication_token }
+      render status: 200, json: { 'success' => 'true', 
+                                  'id' => @user.id, 
+                                  'auth_token' => @user.authentication_token, 
+                                  'email' => @user.email}.to_json
     end
 
   end
