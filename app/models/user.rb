fd6aa82c1b6d69be84a22b1ec3e252fc7f873122
Ahmet Sezgin Duran <marjinalist1@gmail.com>
2013-09-28 16:15:00 +0300

    #2 Add Enumerize gem for role management

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 class User < ActiveRecord::Base
+  extend Enumerize
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
 		 :token_authenticatable
+
+  enumerize :role, in: { user: 1, editor: 2 }
+
+  # Example creation of User with role
+  # new_user = User.create(email: 'blabla@bla.com', password: '12345678', role: :user)
+
+  # Checking role:
+  # new_user.role.editor? (return true or false)
 end
