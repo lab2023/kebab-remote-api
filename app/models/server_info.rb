40d122b91f5ba28987197ed7b3ca2b1449c133b4
Ahmet Sezgin Duran <marjinalist1@gmail.com>
2013-10-04 13:21:35 +0300

    #3 Add comment lines for some attributes

--- app/models/server_info.rb
+++ app/models/server_info.rb
@@ -10,11 +10,14 @@ class ServerInfo < ActiveRecord::Base
     # UN*X-like systems, such as GNU/Linux, BSD, Mac OS X etc.
     # Windows implementation will be added in future 
     disk = Vmstat.disk('/')
+    # CPU load data => Float typed
     self.cpu_load = Vmstat.load_average.five_minutes
+    # All size data => Megabytes (MB)
     self.mem_total = (mem.active_bytes + mem.inactive_bytes + mem.free_bytes + mem.wired_bytes) / 1024**2
     self.mem_used = self.mem_total - (mem.free_bytes / 1024**2)
     self.disk_total = disk.total_bytes / 1024**2
     self.disk_used = (disk.total_bytes - disk.free_bytes) / 1024**2
+    # Uptime data => Minutes
     self.uptime = (Time.now - Vmstat.boot_time).to_i / 60
   end
 
