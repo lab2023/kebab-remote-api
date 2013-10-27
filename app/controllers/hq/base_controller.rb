4611d31e420087ed2f515d4f9751c3be5e3566f9
Ahmet Sezgin Duran <marjinalist1@gmail.com>
2013-10-27 18:01:41 +0200

    Remove everthing

--- app/controllers/hq/base_controller.rb
+++
@@ -1,7 +0,0 @@
-require 'application_responder'
-
-class Hq::BaseController < ActionController::Base
-  before_action :authenticate_admin!
-  self.responder = ApplicationResponder
-  respond_to :html, :json
-end
--- app/controllers/hq/sessions_controller.rb
+++
@@ -1,3 +0,0 @@
-class Hq::SessionsController < Devise::SessionsController
-
-end
