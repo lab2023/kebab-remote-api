18facb0a21cec91d477ce5e44498d358b4272507
Ahmet Sezgin Duran <marjinalist1@gmail.com>
2013-11-15 13:07:12 +0200

    Clean up application controller

--- app/controllers/kebab_remote_api/api/v1/server_info_controller.rb
+++ app/controllers/kebab_remote_api/api/v1/server_info_controller.rb
@@ -2,7 +2,7 @@ module KebabRemoteApi
 	class Api::V1::ServerInfoController < KebabRemoteApi::ApplicationController
    	def get_server_info
     	@info = ServerInfo.new
-    	render status: 200, json: @info.as_json #if @info.save
+    	render status: 200, json: @info.as_json
   	end
 	end
 end
