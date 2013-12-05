569cccdc19717d24433d41c1f48cf3669a92f702
Ahmet Sezgin Duran <marjinalist1@gmail.com>
2013-12-05 23:28:59 +0200

    Fix tab-spaces

--- app/controllers/kebab_remote_api/api/v1/users_controller.rb
+++ app/controllers/kebab_remote_api/api/v1/users_controller.rb
@@ -1,14 +1,14 @@
 module KebabRemoteApi
-	class Api::V1::UsersController < KebabRemoteApi::ApplicationController
-  	respond_to :json
+  class Api::V1::UsersController < KebabRemoteApi::ApplicationController
+    respond_to :json
 
-	  def index
-  	  render status: 200, json: {
-    	  'active_user' => User.with_block_status(:active).count,
-      	'total_user'  => User.all.count,
-   	    'total_user_by_role' => User.role.values.map { |role| { "role_name" => role.to_s,
+    def index
+      render status: 200, json: {
+        'active_user' => User.with_block_status(:active).count,
+        'total_user'  => User.all.count,
+        'total_user_by_role' => User.role.values.map { |role| { "role_name" => role.to_s,
                                                   "count" => User.with_role(role).count } }
-    	}.to_json
-  	end
-	end
+      }.to_json
+    end
+  end
 end
